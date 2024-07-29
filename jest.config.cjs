69b4a9a51443431f512c6df1d4348090cfc4fd5c
Renas Sitdikov <srr.renas@gmail.com>
2024-07-29 16:15:13 +0200

    create project

---
+++ jest.config.cjs
@@ -0,0 +1,17 @@
+process.env.TZ = 'UTC';
+
+/** @type {import('jest').Config} */
+const config = {
+  preset: 'ts-jest',
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  moduleNameMapper: {
+    '@/(.*)$': '<rootDir>/src/$1',
+  },
+  testEnvironment: 'node',
+  transform: {
+    'node_modules/variables/.+\\.(j|t)sx?$': 'ts-jest',
+  },
+  transformIgnorePatterns: ['node_modules/(?!variables/.*)'],
+};
+
+module.exports = config;
